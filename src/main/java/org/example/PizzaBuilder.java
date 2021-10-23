7cc09d06972316d20cfcd71e642f694605e4baa2
betriz-sanchez-flores <beatrizsanchezflores2000@gmail.com>
2021-10-22 22:00:26 -0600

    pizza.Build

--- src/main/java/org/example/PizzaBuilder.java
+++ src/main/java/org/example/PizzaBuilder.java
@@ -13,7 +13,10 @@ public abstract class PizzaBuilder {
             pizza = new Pizza();
         }
 
-       
+        public abstract void ponerNombre();
+        public abstract void crearMasa();
+        public abstract void crearSalsa();
+        public abstract void crearIngredientes();
 
 
     }
