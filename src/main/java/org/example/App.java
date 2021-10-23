df379389fbcf7d7f6a48ecb068ab9e188ec6d53a
betriz-sanchez-flores <beatrizsanchezflores2000@gmail.com>
2021-10-22 21:41:22 -0600

    “Build_Pizza git commit -m “Build_Pizza

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,16 @@ public class App
 {
     public static void main(String[] args) {
         Pizza pizza;
-
+        Cocinero cocinero = new Cocinero();
+        PizzaBuilder bpb = new BarbacoaPizzaBuilder();
+        PizzaBuilder cpb = new CarbonaraPizzaBuilder();
+        cocinero.setPizzaBuilder(bpb);
+        cocinero.crearPizza();
+        pizza = cocinero.getPizza();
+        System.out.println(pizza.toString());
+        cocinero.setPizzaBuilder(cpb);
+        cocinero.crearPizza();
+        pizza = cocinero.getPizza();
+        System.out.println(pizza.toString());
     }
 }
---
+++ src/main/java/org/example/Pizza.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Pizza {
+    private String nombre;
+    private String masa;
+    private String salsa;
+    private String ingredientes;
+
+    public Pizza()
+    {
+
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMasa() {
+        return masa;
+    }
+
+    public void setMasa(String masa) {
+        this.masa = masa;
+    }
+
+    public String getSalsa() {
+        return salsa;
+    }
+
+    public void setSalsa(String salsa) {
+        this.salsa = salsa;
+    }
+
+
+    }
+}
